3a5dafcdc77b08777015ad1287bbed8f214d8f6d
haotian.chen <haotian.chen@cmexpro.com>
2024-09-10 11:16:09 +0800

    pof: lit tailwindcss 组件库

--- examples/swap-ui/src/pages/IndexPage.ts
+++ examples/swap-ui/src/pages/IndexPage.ts
@@ -4,7 +4,7 @@ import { html } from "lit";
 import { customElement } from "lit/decorators.js";
 import { TwLitElement } from "../common/TwLitElement";
 
-// import "../components/HelloWorld";
+import '@eth/swap-ui/index.css';
 import "@eth/swap-ui";
 
 @customElement("x-index-page")
@@ -13,6 +13,7 @@ export class IndexPage extends TwLitElement {
     return html`
       <div class="container">
         <x-hello-world></x-hello-world>
+        这种方案不可行，组件库，单独打出来的css，会被项目中的lit组件隔离掉，导致样式丢失
       </div>
     `;
   }
--- examples/swap-ui/src/util/TailwindMixin.ts
+++ examples/swap-ui/src/util/TailwindMixin.ts
@@ -1,6 +1,5 @@
 import { adoptStyles, LitElement, unsafeCSS } from 'lit'
 
-import style from '../styles/tailwind.global.css' 
 
 declare global {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
@@ -8,12 +7,11 @@ declare global {
 }
 
 
-const stylesheet = unsafeCSS(style)
 
 export const TW = <T extends LitMixin>(superClass: T): T =>
   class extends superClass {
     connectedCallback() {
       super.connectedCallback();
-      adoptStyles(this.shadowRoot, [stylesheet])
+      adoptStyles(this.shadowRoot, [])
     }
   };
